11adf50e88d4a25d97e9ae4d51d2cfe29bd1af88
slipsknuten <gurra.bergqvist@gmail.com>
2025-10-08 16:22:34 +0200

    largest smallest

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
 
 fn main() {
 
+    //odd_or_even();
+    largest_smallest();
+}
+
+fn largest_smallest(){
+    let test_arr = vec![42, 17, 93, -5, 0, 18];
+    let mut largest = test_arr[0];
+    let mut smallest = test_arr[0];
+    for &i in &test_arr {
+        if i > largest {
+            largest = i;
+        }
+        if i < smallest {
+            smallest = i;
+        }
+    }
+
+    println!("Largest: {}", largest);
+    println!("Smallest: {}", smallest);
+}
+
+fn odd_or_even(){
+
     let mut test = vec![1, 2, 3, 4, 5, 6];
     let mut even = vec![];
     let mut odd = vec![];
@@ -15,5 +38,4 @@ fn main() {
 
     println!("Even: {:?}", even);
     println!("Odd: {:?}", odd);
-
 }
